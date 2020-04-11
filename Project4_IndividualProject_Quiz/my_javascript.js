a7410d96facf4ddc670db8b689d7c17f82f76d76
“[firstname <peza361@gmail.com>
2020-04-11 14:48:18 -0400

    big update, loading quiz info and temp

--- Project4_IndividualProject_Quiz/my_javascript.js
+++ Project4_IndividualProject_Quiz/my_javascript.js
@@ -2,60 +2,88 @@
 
 //var firstname = window.prompt( );
 //var lastname;
+
+//var name = 
+//var quizChoice = 
 function submitStart() {
-	var x = document.forms["myForm"]["fullname"].value;
-	if (x == "") {
+	//var name = document.forms["myForm"]["fullname"].value;
+	//var quizChoice = document.forms["myForm"]["QUIZ"].value;
+	var name = document.getElementById("fullname").value;
+	var quizChoice = document.getElementById("QUIZ").value;
+	if (name == "") {
 		alert("Name must be filled out!!!");
-		return  false;
+		return false;
 	}
 	else{
-		document.getElementById("startInput").submit();
+		alert("Hello " + name + "\nQuiz selected: " + quizChoice);
+		//document.getElementById("startInput").submit();
+		document.getElementById("startInput").style.display = "none";
+		document.getElementById("Header").innerHTML = "NAME: " + name + "&nbsp;&nbsp;-------&nbsp;&nbsp;QUIZ SELECTED: " + quizChoice;
+		create_user_view(0)	
+		return false;
 	}
-	
+	//return false;
 }
 
+function checkAnswer() {
+	//var radios = document.forms["submitAnswer"]["choice"].textContent;
+	var radios = document.getElementsByName('choice');
+	for(i = 0; i < radios.length; i++){
+		if(radios[i].checked){
+			alert("You slected" + radios[i].textContent);
+		}
+	}
+	
+	
+}
 //var myInfo = "<p>Question: {{}}
 
 
-
+//main method loads when the page is loaded 
 document.addEventListener('DOMContentLoaded', () => {
-	widget_html = render_view("#post_question_view");
-	document.querySelector("#app_widget").innerHTML = widget_html;
-	
+	//widget_html = render_view("#post_question_view");
+	//document.querySelector("#app_widget").innerHTML = widget_html;
+	//create_user_view(1)	
 });
 
 
-//render template 
-const render_view = (view) => {
+
+//gets data from fake api
+const create_user_view = async (user_idx) => {
+	//const data = await fetch("https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza")
 	
-	template_source = document.querySelector(view).innerHTML
+	const data = await fetch('https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza/JavaQuizQuestions')//
+	const model = await data.json()
+	
+	
+	const html_element = render_widget(model,'#post_question_view')
+	document.querySelector("#app_widget").innerHTML = html_element;
+	console.log(model)
+}
+
+
+
+//render template 
+const render_widget = (model, view) => {
+	
+	template_source = document.querySelector(view).innerHTML //view is the script tags in html that contain your template 
 	//console.log(tempalte_source);
 	// Handlebars compiles the above source into a template
 	var template = Handlebars.compile(template_source);
-	
-	//apply the model to the template.
-	
-	var model = {
-		question : 'Ayayayayaya'
-	}
-	
+
 	console.log(model)
 	//var html_widget_element = template({...modle,...appState})
 	var html_widget_element = template(model)
-	
+	console.log(html_widget_element)
 	return html_widget_element
 	
-	
-}
-
-//gets data from fake api
-
-const create_user_view = async (user_idx) => {
-	//const data = await fetch("https://jsonplaceholder.typicode.com/photos/1")
-	const data = await fetch("url")
-	const model = await data.json()
-	const html_element = render_widget(model, '#user_view')
-	document.querySelector("app_widget").innerHTML = html_element;
 }
 
 
+//document.getElementById("form_id").submit();// Form submission	
+	//apply the model to the template.
+/*	
+	var model = {
+		question : 'Ayayayayaya'
+	}
+*/	
