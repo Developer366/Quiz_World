e12873015ccea12f7f1b83be416eb492531c1421
“[firstname <peza361@gmail.com>
2020-04-13 19:14:46 -0400

    big update 2

--- Project4_IndividualProject_Quiz/my_javascript.js
+++ Project4_IndividualProject_Quiz/my_javascript.js
@@ -1,13 +1,17 @@
 //This is a file called my_javascript.js
 //Global Variables
-
+//const data = await fetch('https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza/db')
+//const model = await data.json()//model stores the databse
+var score = 0; 
 var i = 0;
+//var questionNum= 1;
+var quizSelection;
 
 function submitStart() {
 	//var name = document.forms["myForm"]["fullname"].value;
 	//var quizChoice = document.forms["myForm"]["QUIZ"].value;
 	var name = document.getElementById("fullname").value;
-	var quizChoice = document.getElementById("QUIZ").value;
+	var quizChoice = document.getElementById("QuizChoice").value;
 	if (name == "") {
 		alert("Name must be filled out!!!");
 		return false;
@@ -17,18 +21,81 @@ function submitStart() {
 		//document.getElementById("startInput").submit();
 		document.getElementById("startInput").style.display = "none";
 		document.getElementById("Header").innerHTML = "NAME: " + name + "&nbsp;&nbsp;-------&nbsp;&nbsp;QUIZ SELECTED: " + quizChoice;
-		create_user_view(i)	
+		//create_user_view(1)
+		if(quizChoice == "Java Quiz"){
+			quizSelection ="Java Quiz";
+			var i = 0;
+			create_user_view_Q1(i)
+		}
+		else if(quizChoice== "HTML/CSS Quiz"){
+			quizSelection ="HTML/CSS Quiz";
+			var j = 0;
+			create_user_view_Q2(j)
+		}
+		
+		 //"Java_Quiz"
 		return false;
 	}
 	//return false;
 }
 //check answer if right good job next question. iF WRONG EXPLAIN AND THEN CLICK OK and move on.
 function checkAnswer() {
-	var option1 = document.getElementById("option1");
-	var option2 = document.getElementById("option2");
-	var option3 = document.getElementById("option3");
-	var option4 = document.getElementById("option4"); 
-
+		
+	var i = 0;
+	var list = document.getElementsByName("choice");
+	var answer = document.getElementById("answer").innerHTML; 	
+	console.log(list)
+		for(var i = 0; i < list.length; i++){
+			//var checkedAnswer = x[i].checked;
+			if(list[i].checked == true){			
+				console.log(answer)
+				if(list[i].value==answer){
+					console.log(list[i].value)
+					document.getElementById("feedBackView").style.display = "block";
+					document.getElementById("correctAnswer").style.display = "block";	
+					setTimeout(nextQuestion,2000);
+						/*
+						if(quizChoice=="Java Quiz"){
+							create_user_view_Q1(i)
+						}
+						else if(quizChoice=="HTML/CSS Quiz"){
+							create_user_view_Q2(j)
+						}
+						*/
+					//console.log("asdsasd")
+				}
+				else {
+					document.getElementById("feedBackView").style.display = "block";
+					document.getElementById("wrongAnswer").style.display = "block";
+					document.getElementById("OK").addEventListener("click",nextQuestion);
+					
+					//console.log("dfdsdf")
+					/*
+					if(quizChoice=="Java Quiz"){
+							create_user_view_Q1(i)
+						}
+						else if(quizChoice=="HTML/CSS Quiz"){
+							create_user_view_Q2(j)
+						}
+					*/
+				}			
+			}			
+		}
+	}
+	
+	
+function nextQuestion(){
+	if(quizSelection=="Java Quiz"){
+		i++;
+		create_user_view_Q1(i)
+	}
+	else if(quizSelection=="HTML/CSS Quiz"){
+		j++;
+		create_user_view_Q2(j)
+	}
+	
+}
+	
 	
 /*	
 	if(){
@@ -51,9 +118,16 @@ function checkAnswer() {
 			alert("You slected" + radios[i].textContent);
 		}
 	}
+	var option1 = document.getElementById("option1");
+	var option2 = document.getElementById("option2");
+	var option3 = document.getElementById("option3");
+	var option4 = document.getElementById("option4"); 
+	
+	
+	
 	
 */	
-}
+
 
 
 //main method loads when the page is loaded 
@@ -66,19 +140,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //gets data from fake api
-const create_user_view = async (index) => {
+//var quizSelection;
+
+//var questionNum = 0;
+//var quizSelection;
+//create_user_view(Java_Quiz, 0)
+//Java Quiz
+console.log()
+const create_user_view_Q1 = async (index) => {
 	//const data = await fetch("https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza")
-	
-	const data = await fetch('https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza/db')//
-	const model = await data.json()
-		//document.getElementById()
-	
-	const html_element = render_widget(model,'#multipleChoice')
-	document.querySelector("#app_widget").innerHTML = html_element;
-	//console.log(model[index])
+	var i=1;
+	const data = await fetch('https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza/db')
+	const model = await data.json()//model stores the databse
+	const html_element = render_widget(model.Java_Quiz[index],'#multipleChoice')//renders the view with handlebars
+//const html_element = render_widget(model.Java_Quiz[0],'#multipleChoice')//renders the view with handlebars
+
+	document.querySelector("#app_widget").innerHTML = html_element; //selects where to render the template
+	//var a = 1;
+	//console.log(model.Java_Quiz[a])
 }
+//HTML CSS QUIZ
+const create_user_view_Q2 = async (index) => {
+	//const data = await fetch("https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza")
+	var i=1;
+	const data = await fetch('https://my-json-server.typicode.com/Developer366/CUS1172_Spring2020_Kamil_Peza/db')
+	const model = await data.json()//model stores the databse
+	const html_element = render_widget(model.HTML_CSS_QUIZ[index],'#multipleChoice')//renders the view with handlebars
+//const html_element = render_widget(model.Java_Quiz[0],'#multipleChoice')//renders the view with handlebars
 
-
+	document.querySelector("#app_widget").innerHTML = html_element; //selects where to render the template
+	var a = 1;
+	console.log(model.Java_Quiz[a])
+}
 
 
 //render template 
@@ -88,9 +181,8 @@ const render_widget = (model, view) => {
 	//console.log(tempalte_source);
 	// Handlebars compiles the above source into a template
 	var template = Handlebars.compile(template_source);
-
-	console.log(model)
-	//var html_widget_element = template({...modle,...appState})
+	
+	//var html_widget_element = template({...modle,...appState}
 	var html_widget_element = template(model)
 	console.log(html_widget_element)
 	return html_widget_element
